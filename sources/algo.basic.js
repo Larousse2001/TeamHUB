6de0ec6541e025f3c05cc658c8ae32fc0b808ed4
mariem100 <bouchaalamariem2001@gmail.com>
2023-11-17 13:49:56 +0100

    dbdzdz

--- sources/algo.basic.js
+++
@@ -1,9 +0,0 @@
-/** 
- * No special treatment.
-  *waaa
-  * Sorts unassigned variables according to their initial order.
-  * eg: [0:0, 0:1, 0:2, ..., 8:8]
- */
-  function basic(assignement, unassigned) {
-    return sortByName(unassigned)
-  }
